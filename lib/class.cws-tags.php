d16b308c5d8cf63fcb2a2bda6df1dfdabd8c44c4
psmagicman <julienlaw1@gmail.com>
2013-06-27 14:54:32 -0700

    Added function descriptions for future development

--- lib/class.cws-tags.php
+++ lib/class.cws-tags.php
@@ -7,16 +7,40 @@ class Combined_Wiki_Search_Tags {
 	
 	static function tags_shortcode() {
 		ob_start();
-
+		self::create_area();
 		$buffer = ob_get_contents();
 		ob_end_clean();
 		return $buffer;
 	}
 
-	static function generate_page() {
-		
-	}	
+	/**
+	generate_page function
+	This function will create an area for the tags
+	*/
+	static function create_area() {
+		?>
+		<h3>Tag Space</h3>
+		<?php
+	}
 
+	/**
+	get_tags function
+	This function will generate a list of tags and return an array of the tags
+	@return array
+	*/
+	static function get_tags() {
+		$tags = new Array();
+		
+		return $tags;
+	}
+
+	/**
+	pass_info function
+	This function will pass the information contained in the tag as POST data
+	*/
+	static function pass_info() {
+
+	}
 }
 
 Combined_Wiki_Search_Tags::init();
