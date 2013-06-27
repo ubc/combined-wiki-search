e3a44b1f77d643d80fa496f061a48d0d13ec1da9
psmagicman <julienlaw1@gmail.com>
2013-06-27 14:42:37 -0700

    prepare for tags code

--- lib/class.cws-tags.php
+++ lib/class.cws-tags.php
@@ -1,16 +1,22 @@
 <?php
 class Combined_Wiki_Search_Tags {
+
 	static function init() {
 		add_shortcode( 'cws_tags', array( __CLASS__, 'tags_shortcode' ) );
 	}
 	
 	static function tags_shortcode() {
 		ob_start();
-		?>
-		Tags
-		<?php
-		return ob_get_clean();
+
+		$buffer = ob_get_contents();
+		ob_end_clean();
+		return $buffer;
 	}
+
+	static function generate_page() {
+		
+	}	
+
 }
 
 Combined_Wiki_Search_Tags::init();
